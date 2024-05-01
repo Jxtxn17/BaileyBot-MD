c741c2ad745412e4aea4a6bfeba398ac833827e9
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 13:41:35 -0500

    Actualizar ai-hd.js

--- plugins/ai-hd.js
+++ plugins/ai-hd.js
@@ -5,30 +5,30 @@ var handler = async (m, { conn, usedPrefix, command }) => {
 
 conn.hdr = conn.hdr ? conn.hdr : {}
 if (m.sender in conn.hdr)
-return conn.reply(m.chat, `*🏴 Todavía hay una solicitud en proceso, sea paciente*`, m, fake, )
+return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 𝗍᥆ძᥲ᥎і́ᥲ һᥲᥡ ᥙᥒᥲ s᥆ᥣіᥴі𝗍ᥙძ ᥱᥒ ⍴r᥆ᥴᥱs᥆, sᥱᥲ ⍴ᥲᥴіᥱᥒ𝗍ᥱ*`, m, fake, )
 
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ""
 if (!mime)
 
-return conn.reply(m.chat, `*🎌 Etiquete a una foto*`, m, fake, )
+return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥱ𝗍і𝗊ᥙᥱ𝗍ᥱ ᥲ ᥙᥒᥲ 𝖿᥆𝗍᥆*`, m, fake, )
 if (!/image\/(jpe?g|png|mp4)/.test(mime))
 
-return conn.reply(m.chat, `*🚩 Mímica ${mime} no soportada*`, m, fake, )
+return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 mі́mіᥴᥲ ${mime} ᥒ᥆ soportada*`, m, fake, )
 else conn.hdr[m.sender] = true
 
-conn.reply(m.chat, '⏰ Espere un momento', m, fake, )
+conn.reply(m.chat, '🚫 𝐀𝐓𝐄𝐍𝐂𝐈𝐎́𝐍 🚫 ᥱs⍴ᥱrᥱ ᥙᥒ m᥆mᥱᥒ𝗍᥆', m, fake, )
 let img = await q.download?.()
 
 let error
 try {
 const This = await processing(img, "enlace")
-conn.sendFile(m.chat, This, '', '🧃 *Toma tu foto*\n\n' + cred.toString('utf-8'), m)
+conn.sendFile(m.chat, This, '', '🧃 *𝗍᥆mᥲ 𝗍ᥙ 𝖿᥆𝗍᥆*\n\n' + cred.toString('utf-8'), m)
 } catch (er) {
 error = true
 } finally {
 if (error) {
-return conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m, fake, )
+return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥆ᥴᥙrrі᥆ ᥙᥒ 𝖿ᥲᥣᥣ᥆*`, m, fake, )
 }
 delete conn.hdr[m.sender]
 }}
