f248956c2770f1b40e48c1c208fcc88bddef5357
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 12:56:50 -0500

    Actualizar bot-bot.js

--- plugins/bot-bot.js
+++ plugins/bot-bot.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) {
 
 conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, '🎌 *Estoy aquí para ayudarte*', m, fake, )}
+conn.reply(m.chat, '*Hola, soy BaileyBot-MD*\n*Estoy aquí para ayudarte*', m, fake, )}
 
 return !0
 
