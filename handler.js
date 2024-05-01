868f36d0624bb20981c782bb000401c04d494e87
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 14:37:17 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -229,7 +229,7 @@ console.error(e)
 const isROOwner = [conn.decodeJid(global.conn.user.id), ...global.isdev.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
 const isROwner = [conn.decodeJid(global.conn.user.id), ...global.owner.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
 const isOwner = isROwner || m.fromMe
-const isDev = isROOwner || m.fromMe
+const isDev = isROwner || m.fromMe
 const isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
 const isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
 
