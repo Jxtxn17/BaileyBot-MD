d0a07badde725531724df4bf295f3200c2051255
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 14:37:51 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,43 +1,52 @@
-/* ⚙️ Credits to:
-* AzamiJs
-* Elrebelde21
-* GataNina-Li */
+import { watchFile, unwatchFile } from 'fs' 
+ import chalk from 'chalk' 
+ import fs from 'fs' 
+ import cheerio from 'cheerio' 
+ import fetch from 'node-fetch' 
+ import axios from 'axios' 
+ import path, { join } from 'path' 
+ import { fileURLToPath, pathToFileURL } from 'url' 
+ import moment from 'moment-timezone'  
+ import { platform } from 'process' 
+ global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) } 
+ const __dirname = global.__dirname(import.meta.url) 
 
-import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
-import { fileURLToPath, pathToFileURL } from 'url'
+ global.owner = [
+['51929972576', '🍧𝙹𝚇𝚃𝚇𝙽¹⁷🍧', true],
+['51955918117', 'Daniel', true],
+['50558124470'],
+['573106040746', '𝙴𝙳𝙴𝚁', true],
+['51992004117', 'HJ', true],
+['5214434703586'],
+['50499698072'],
+['5492215034412'],
+['5217294888993'],
+['5214437863111'],
+['51971959440']]
 
-global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['447700168473', '👑 𝐎𝐰𝐧𝐞𝐫 👑', true], /* ['5217294888993'], */ ['5214531287294'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['59162714296'], ['5492964650915'], ['50558124470'], ['5214434703586'], ['18293132203'], ['573003025991']]
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces: 
+ //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos) 
+ global.botNumberCode = '' //Ejemplo: +59309090909 
+ global.confirmCode = ''
 
-//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
-global.confirmCode = ''
+ global.animxscans = ['51929972576'] 
+ global.suittag = ['51929972576']
+ global.mods = [] 
+ global.prems = []
 
-global.suittag = ['5214531287294']
-global.mods = []
-global.prems = []
+global.packname = 'ᥫ᭡𝘽𝙖𝙞𝙡𝙚𝙮𝘽𝙤𝙩-𝙈𝘿ꨄ'
+global.author = 'Jxtxn17'
+global.wm = '𓆩𝐁𝐚𝐢𝐥𝐞𝐲𝐁𝐨𝐭-𝐌𝐃𓆪'
+global.wm2 = '✦𝘽𝙖𝙞𝙡𝙚𝙮-𝗯𝗼𝘁-𝗠𝗗✦'
+global.jxtxn = 'Jxtxn'
+global.cb = '⏤͟͟͞͞ＢａｉｌｅｙＢｏｔ-ＭＤ'
 
-//Solo desarrolladores aprobados
-global.isdev = [ /* ['5217294888993'], */ ['5214434703586'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['5492964650915'], ['50558124470']]
-
-//Aqui encuentras los nombres del bot 
-global.packname = ''
-global.author = 'CuriosityBot'
-global.wm = '© CuriosityBot-MD'
-global.wm2 = 'Curiosity : Bot'
-global.azami = 'Azami'
-global.cb = 'CuriosityBot'
-
-
-//Las Versiones Y Entre Otros
-global.vs = 'V2'
+global.vs = '1.0.2'
 global.library = 'Baileys'
 global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = 'Jxtxn ~ Axx'
-global.devnum = 'ʙʏ ᴀxx.ᴏғᴄ'
+global.dev = '51929972576'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
