83618e51208bb13682bc615b9422d241b5df9a93
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 20:22:37 -0500

    Update grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,12 +1,30 @@
 var handler = async (m, {conn, args, usedPrefix, command}) => {
 
-const isClose = { 'open': 'not_announcement', 'close': 'announcement', 'abierto': 'not_announcement', 'cerrado': 'announcement', 'abrir': 'not_announcement', 'cerrar': 'announcement', 'desbloquear': 'unlocked', 'bloquear': 'locked' }[(args[0] || '')]
+const isClose = {
+'open': 'not_announcement',
+'close': 'announcement',
+'abierto': 'not_announcement',
+'cerrado': 'announcement',
+'abrir': 'not_announcement',
+'cerrar': 'announcement',
+'desbloquear': 'unlocked',
+'bloquear': 'locked',
+}[(args[0] || '')];
+if (isClose === undefined) {
+throw `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 𝖿᥆rmᥲ𝗍᥆ ᥱrr᥆́ᥒᥱ᥆*\n\n [ 💡 ] ᥱȷᥱm⍴ᥣ᥆:
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━
+┃ *○ ${usedPrefix + command} abrir*
 
-if (isClose === undefined) { return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abrir*\n*○ !${command} cerrar*\n*○ !${command} bloquear*\n*○ !${command} desbloquear*`, m, fake, )
+┃ *○ ${usedPrefix + command} cerrar*
 
+┃ *○ ${usedPrefix + command} bloquear*
+
+┃ *○ ${usedPrefix + command} desbloquear*
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━
+`.trim()
 }
 await conn.groupSettingUpdate(m.chat, isClose)
-{ conn.reply(m.chat, '✅ *Configurado correctamente*', m, fake, ) }
+{m.reply('*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 grᥙ⍴᥆ ᥴᥱrrᥲძ᥆, s᥆ᥣ᥆ ᥲძmіᥒs ⍴ᥙᥱძᥱᥒ ᥱsᥴrіᑲіr.*')}
 
 }
 handler.help = ['group open / close', 'grupo abrir / cerrar']
