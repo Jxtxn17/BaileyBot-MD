9a32d02f027e7ce882056c1fdf641ee33f81db03
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 21:55:18 -0500

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -1,78 +1,72 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
-import os from 'os'
-import util from 'util'
-import sizeFormatter from 'human-readable'
-import MessageType from '@whiskeysockets/baileys'
-import fs from 'fs'
-import { performance } from 'perf_hooks'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys' 
+ import os from 'os' 
+ import util from 'util' 
+ import sizeFormatter from 'human-readable' 
+ import MessageType from '@whiskeysockets/baileys' 
+ import fs from 'fs' 
+ import { performance } from 'perf_hooks' 
+ let handler = async (m, { conn, usedPrefix }) => { 
+ let _uptime = process.uptime() * 1000 
+ let uptime = clockString(_uptime)  
+ let totalreg = Object.keys(global.db.data.users).length 
+ const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats) 
+ const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) 
+ const groups = chats.filter(([id]) => id.endsWith('@g.us')) 
+ const used = process.memoryUsage() 
+ const { restrict, antiCall, antiprivado, modejadibot } = global.db.data.settings[conn.user.jid] || {} 
+ const { autoread, gconly, pconly, self } = global.opts || {} 
+ let old = performance.now() 
+ let neww = performance.now() 
+ let pp = './storage/logos/Menu1.jpg' 
+ let speed = neww - old 
+ let info = `┏━━━━━━━━━━━━━━━━━
+┃ ꨄ *CREADOR* 
+┃ *Jxtxn17*
+ 
+┃  ꨄ *CONTACTO*  
+┃  *${ig}*
 
-var handler = async (m, { conn, usedPrefix }) => {
+┃ ꨄ *VERSIÓN ACTUAL* 
+┃  ${vs} 
+ 
+┃ ꨄ *PREFIJO* 
+┃ *${usedPrefix}* 
 
-let _uptime = process.uptime() * 1000
-let uptime = clockString(_uptime) 
-let totalreg = Object.keys(global.db.data.users).length
+┃  ꨄ *CHATS PRIVADOS* 
+┃  *${chats.length - groups.length}* 
+ 
+┃  ꨄ *CHATS GRUPALES* 
+┃ *${groups.length}*  
+ 
+┃ ꨄ *CHATS EN TOTAL* 
+┃ *${chats.length}*  
 
-const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
-const groups = chats.filter(([id]) => id.endsWith('@g.us'))
-const used = process.memoryUsage()
-const { restrict, antiCall, antiprivado, modejadibot } = global.db.data.settings[conn.user.jid] || {}
-const { autoread, gconly, pconly, self } = global.opts || {}
-
-let old = performance.now()
-let neww = performance.now()
-let speed = neww - old
-
-let info = `⬡ *Creador*
-Azami
-
-⬡ *Contacto* 
-${ig}
-
-⬡ *Versión actual*
- ${vs}
-
-⬡ *Prefijo*
-!
-
-⬡ *Chats privados*
-${chats.length - groups.length}
-
-⬡ *Chats grupales*
-${groups.length}
-
-⬡ *Todos los chats*
-${chats.length}
-
-⬡ *Actividad*
-${uptime}
-
-⬡ *Usuarios*
-${totalreg}
-
-⬡ *Velocidad*
-${speed}
-
-⬡ *Autoread*
-${autoread ? 'Habilitado' : 'Deshabilitado'}
-
-⬡ *Restrict*
-${restrict ? 'Habilitado' : 'Deshabilitado'}`
-
- conn.sendMessage(m.chat, { text: info, contextInfo: { externalAdReply: { title: '', body: '🚩 Curiosity Info', thumbnail: imagen2, sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
-
-}
-handler.help = ['infobot']
-handler.tags = ['info']
-handler.command = /^(infobot|Infobot)$/i
-
-handler.register = true
-
-export default handler
-
-function clockString(ms) {
-let h = Math.floor(ms / 3600000)
-let m = Math.floor(ms / 60000) % 60
-let s = Math.floor(ms / 1000) % 60
-console.log({ms,h,m,s})
-return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
+┃  ꨄ *ACTIVIDAD* 
+┃ *${uptime}* 
+ 
+┃ ꨄ *USUARIOS* 
+┃ *${totalreg}*  
+ 
+┃  ꨄ *VELOCIDAD:* 
+┃  *${speed}* 
+ 
+┃ ꨄ *AUTOREAD:* 
+┃ ${autoread ? '*Habilitado ✅*' : '*Deshabilitado ❌*'} 
+ 
+┃  ꨄ *RESTRICT:* 
+┃ ${restrict ? '*Habilitado ✅*' : '*Deshabilitado ❌*'}
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━
+`.trim()  
+  conn.sendMessage(m.chat, { text: info, contextInfo: { externalAdReply: { title: '', body: 'BaileyBot-MD 🗿', thumbnail: imagen2, sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+ } 
+ handler.help = ['infobot', 'speed'] 
+ handler.tags = ['main'] 
+ handler.command = /^(infobot|Infobot)$/i 
+ export default handler 
+  
+ function clockString(ms) { 
+ let h = Math.floor(ms / 3600000) 
+ let m = Math.floor(ms / 60000) % 60 
+ let s = Math.floor(ms / 1000) % 60 
+ console.log({ms,h,m,s}) 
+ return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
