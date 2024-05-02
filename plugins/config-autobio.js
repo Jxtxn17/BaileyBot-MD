07037e396a8d5cc7c0e6960a5bb97c6214689055
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 21:12:57 -0500

    Update config-autobio.js

--- plugins/config-autobio.js
+++ plugins/config-autobio.js
@@ -1,26 +1,25 @@
-/*let handler = m => m
-handler.all = async function (m) {
-
-let setting = global.db.data.settings[this.user.jid]
-	
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 2000) }) * 1000}
-let uptime = clockString(_uptime)
-let bio = `${wm} ║ ⏰ Actividad:  ${uptime}`
-await this.updateProfileStatus(bio).catch(_ => _)
-setting.status = new Date() * 1
-
-} 
-export default handler
-
-function clockString(ms) {
-let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [d, 'd ️', h, 'h ', m, 'm ', s, 's '].map((v) => v.toString().padStart(2, 0)).join('')
-}
-*/
+let handler = m => m 
+ handler.all = async function (m) { 
+ let setting = global.db.data.settings[this.user.jid] 
+  
+ let _uptime = process.uptime() * 1000 
+ let _muptime 
+ if (process.send) { process.send('uptime') 
+ _muptime = await new Promise(resolve => { process.once('message', resolve)  
+ setTimeout(resolve, 2000) }) * 1000} 
+ let uptime = clockString(_uptime) 
+ let bio = `${global.wm} ║
+ ${uptime}
+.estado .menu .jadibot. .owner ║ 𝙱𝚢 𝙹𝚡𝚝𝚡𝚗𝟷𝟽` 
+ await this.updateProfileStatus(bio).catch(_ => _) 
+ setting.status = new Date() * 1 
+ }  
+ export default handler 
+  
+ function clockString(ms) { 
+   let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000) 
+   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24 
+   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60 
+   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60 
+   return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('')  
+ } 
