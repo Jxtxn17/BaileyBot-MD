a561ab7ca8b8276c62b777341336643f82d4a832
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 20:21:38 -0500

    Update grupo-admins.js

--- plugins/grupo-admins.js
+++ plugins/grupo-admins.js
@@ -1,26 +1,26 @@
 var handler = async (m, { conn, participants, groupMetadata, args, text }) => {
 
-let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './storage/logos/Menu2.jpg'
+let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './storage/menus/Menu2.jpg'
 let groupAdmins = participants.filter(p => p.admin)
 let listaAdmins = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 let owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
-if (!text) return conn.reply(m.chat, `🎌 *Por favor coloque su motivo para invocar a los admins*`, m, fake, )
-if (text.length < 10) return conn.reply(m.chat, `🚩 *El motivo es muy corto, minimo 10 carácteres*`, m, fake, )
+if (!text) return m.reply(`*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥴ᥆ᥣ᥆𝗊ᥙᥱ ᥱᥣ m᥆𝗍і᥎᥆ ⍴ᥲrᥲ іᥒ᥎᥆ᥴᥲr ᥲ ᥣ᥆s ᥲძmіᥒs*`)
+if (text.length < 10) return m.reply(`*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥱᥣ m᥆𝗍і᥎᥆ ᥱs mᥙᥡ ᥴ᥆r𝗍᥆, mіᥒіm᥆ 10 ᥴᥲrᥲᥴ𝗍ᥱrᥱs*`)
 let mensaje = args.join` `
-let yo = `*Mensaje:* ${text}`
-let texto = `${yo}
+let yo = `*📢 mᥱᥒsᥲȷᥱ:* ${text}`
+let texto = `*༺ іᥒ᥎᥆ᥴᥲᥒძ᥆ ᥲძmіᥒs ༻*
 
-🚩 *Staff Del Grupo*
+${yo}
+
+*❗ s𝗍ᥲ𝖿𝖿 ძᥱᥣ grᥙ⍴᥆❗*
 ${listaAdmins}`.trim()
-
 conn.sendFile(m.chat, pp, 'error.jpg', texto, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 
 }
 handler.help = ['admins']
 handler.tags = ['grupo']
 handler.command = /^(admins|@admins|dmins)$/i
-
 handler.group = true
 
 export default handler
