83118caa54c0db83b40d1791c941b6613ec104ce
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-01 20:22:59 -0500

    Update grupo-demote.js

--- plugins/grupo-demote.js
+++ plugins/grupo-demote.js
@@ -1,35 +1,33 @@
 var handler = async (m, { conn,usedPrefix, command, text }) => {
 
-if (isNaN(text) && !text.match(/@/g)){
+if(isNaN(text) && !text.match(/@/g)){
 	
-} else if (isNaN(text)) {
+}else if(isNaN(text)) {
 var number = text.split`@`[1]
-} else if (!isNaN(text)) {
+}else if(!isNaN(text)) {
 var number = text
 }
-
-if (!text && !m.quoted) return conn.reply(m.chat, `🎌 *Etiqueta al admin. que desea degradar*\n\nEjemplo, !demote @tag\n!demote *responde a un mensaje*`, m, fake, )
-if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚩 *Número erróneo*`, m, fake, )
+if(!text && !m.quoted) return conn.reply(m.chat, `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥱȷᥱm⍴ᥣ᥆ ძᥱ ᥙs᥆*\n\n*${usedPrefix}demote @tag*\n*${usedPrefix}demote responder a un mensaje*`, m)
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥒᥙmᥱr᥆ іᥒᥴ᥆rrᥱᥴ𝗍᥆`, m)
   
 try {
-if (text) {
+if(text) {
 var user = number + '@s.whatsapp.net'
-} else if (m.quoted.sender) {
+} else if(m.quoted.sender) {
 var user = m.quoted.sender
-} else if (m.mentionedJid) {
+} else if(m.mentionedJid) {
 var user = number + '@s.whatsapp.net'
 } 
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-conn.reply(m.chat, `✅ *Se degrado de admin con éxito*`, m, fake, )
+m.reply(`*✅ ᥱ᥊і𝗍᥆, ᥡᥲ ᥒ᥆ 𝗍іᥱᥒᥱ ⍴᥆ძᥱr ᥱᥒ ᥱs𝗍ᥱ grᥙ⍴᥆*`)
 }
 
 }
 handler.help = ['demote']
 handler.tags = ['grupo']
-handler.command = ['demote', 'degradar']
-
+handler.command = ['demote', 'degradar'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
