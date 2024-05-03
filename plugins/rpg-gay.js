7ff886266d235757845b07eb19c29dca90a6c405
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-03 13:03:29 -0500

    Actualizar rpg-gay.js

--- plugins/rpg-gay.js
+++ plugins/rpg-gay.js
@@ -3,7 +3,7 @@ let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.m
 let name = conn.getName(who)
 const pp = './storage/logos/Menu2.jpg'
 
-let gay = `
+let love = `
 ┌─────⊱◈◈◈⊰─────
 ┃🚩 Nombre: ${name} 
 ┃🏳‍🌈: *${Math.floor(Math.random() * 100)}%* Gay
