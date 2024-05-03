034babab2d540364e8a6b773c473ce5f0af8894f
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-03 13:02:04 -0500

    Actualizar plugins

---
+++ plugins/rpg-gay.js
@@ -0,0 +1,23 @@
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = conn.getName(who)
+const pp = './storage/logos/Menu2.jpg'
+
+let love = `
+┌─────⊱◈◈◈⊰─────
+┃🚩 Nombre: ${name} 
+┃🏳‍🌈: *${Math.floor(Math.random() * 100)}%* Gay
+└─────⊱◈◈◈⊰─────`
+
+conn.sendMessage(m.chat, {text: love, mentions: [m.sender]}, {quoted: m})
+
+//conn.reply(m.chat, `*🚩 Error Del Sistema*`, m, fake, )
+
+}
+handler.help = ['love']
+handler.tags = ['juegos']
+handler.command = /^(love|amor|Love)$/i
+
+handler.register = true
+
+export default handler
