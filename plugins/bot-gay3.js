871ed315c82f5549f7f29caddcce740167b4b5c9
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-05 13:16:45 -0500

    Actualizar bot-gay3.js

--- plugins/bot-gay3.js
+++ plugins/bot-gay3.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 if (/^Hola|ola$/i.test(m.text) && !chat.isBanned) {
 
 conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, '*Hola, Como éstas?*', m, fake, )}
+conn.reply(m.chat, '*Hola, Como éstas?*\n*Pon .allmenu para ver mi lista de comandos*', m, fake, )}
 
 return !0
 
