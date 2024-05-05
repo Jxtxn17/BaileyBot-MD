b9522fcdf5b984a200bc3100bfbb5e8cecbd5718
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 20:44:35 -0500

    Actualizar config-antilink.js

--- plugins/config-antilink.js
+++ plugins/config-antilink.js
@@ -10,11 +10,11 @@ let delet = m.key.participant
 let bang = m.key.id
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
-const grupo = `https://chat.whatsapp.com`
+const grupo = `https://whatsapp.com/channel`
 if (isAdmin && chat.antiLink && m.text.includes(grupo)) return conn.reply(m.chat, `🎌 *Hey!! el anti link esta activo pero eres admin, ¡salvado!*`, m, fake, )
 if (chat.antiLink && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
-const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+const linkThisGroup = `https://whatsapp.com/channel/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }
 await conn.reply(m.chat, `🚩 *¡Enlace detectado!*\n\n*${await this.getName(m.sender)} mandaste un enlace prohibido por lo cual seras eliminado*`, m, fake, )
