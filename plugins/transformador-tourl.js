b1590b5898beb79c787d9a1a04baadff8b17c6ee
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 22:52:49 -0500

    Actualizar transformador-tourl.js

--- plugins/transformador-tourl.js
+++ plugins/transformador-tourl.js
@@ -1,38 +1,15 @@
-/*Créditos a https://github.com/AzamiJs*/
-
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import fetch from 'node-fetch'
-
-var handler = async (m) => {
-
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-let name = await conn.getName(who)
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '🚩 Responda a una imagen o un video'
-m.react(done)
-let media = await q.download()
-let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-let link = await (isTele ? uploadImage : uploadFile)(media)
-let info = ` *😎 Enlace:*\n${link}\n
-*💻 Tamaño:*\n${media.length} bytes\n
-*☄️ Expiración:*\n📱 No Expira\n
-*🌻 Acortado:*\n${await shortUrl(link)}`
-
-conn.reply(m.chat, info, m, { contextInfo: { externalAdReply :{ mediaUrl: md, mediaType: 2, title: wm, body: nombre, thumbnail: await(await fetch(link)).buffer(), sourceUrl: md}}})
-
-}
-handler.help = ['tourl']
-handler.tags = ['transformador']
-handler.command = /^(tourl|upload)$/i
-
-handler.limit = true
-
-export default handler
-
-async function shortUrl(url) {
-let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-return await res.text()
-}
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!mime) throw '*Y LA IMAGEN?*';
+  const media = await q.download();
+  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+  const link = await (isTele ? uploadImage : uploadFile)(media);
+  m.reply(`${link}`);
+};
+handler.help = ['tourl <reply image>'];
+handler.tags = ['sticker'];
+handler.command = /^(upload|tourl)$/i;
+export default handler;
