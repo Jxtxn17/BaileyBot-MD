c179d4318684e7157e2a4c104dec7ada79cdd59b
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-05 12:13:42 -0500

    Actualizar bot-menu.js

--- plugins/bot-menu.js
+++ plugins/bot-menu.js
@@ -19,7 +19,7 @@ try {
 
 let user = conn.getName(m.sender)
 
-let media = './storage/logos/Menu1.jpg' 
+let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 let fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}}
 let links = linkSity.getRandom()
 
@@ -28,26 +28,32 @@ let fechaMoment, formatDate, nombreLugar, ciudad = null; const phoneNumber = '+'
 //FAKES
 let a = {'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '3B64558B07848BD81108C1D14712018E'}, 'message': {'locationMessage': {'name': `${user}`, 'jpegThumbnail': await (await fetch(pp)).buffer(), 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
 const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `${user}!`, orderTitle: `▮Menu ▸`, sellerJid: '0@s.whatsapp.net' }}}
-const fload = { key : { message: `BaileyBot-MD ☄️` + `\nAxx`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' }}
+const fload = { key : { message: `CuriosityBot-MD 🍧` + `\nAzami`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' }}
 
-m.react('🐕')
+m.react('🏷️')
 
-let menu = `*👋 Hola, ${nombre}*
+let menu = `*👋 Hola, ${user}*
+
+_${formatDate}_
 
 *País*: ${nombreLugar} 
+*Ciudad*: ${ciudad}
 
 !allmenu
 _(Para ver el menú completo)_
 
-await conn.reply(m.chat, menu, a, { contextInfo: { externalAdReply: {title: '📱 ¡Saludo!', body: wm, sourceUrl: md, thumbnail: await (await fetch(pp)).buffer() }}})
+!menu2
+_(Para ver el menu audios)_`
+
+await conn.reply(m.chat, menu, a, { contextInfo: { externalAdReply: {title: '👋 ¡Hola!', body: saludo, sourceUrl: ig, thumbnail: await (await fetch(pp)).buffer() }}})
 
 } catch {
-conn.reply(m.chat, `*🌼 Ocurrió un fallo*`, m, fake, )
+conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m, fake, )
 console.log(e)
 }}
 handler.help = ['menu']
 handler.tags = ['bot']
-handler.command = /^(botsito)$/i
+handler.command = /^(menu)$/i
 handler.register = true
 
 export default handler
