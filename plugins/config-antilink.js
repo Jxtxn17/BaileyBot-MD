90e24a767d54b06971f585809da6c00a2cb6215c
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-04 20:36:40 -0500

    Actualizar config-antilink.js

--- plugins/config-antilink.js
+++ plugins/config-antilink.js
@@ -1,4 +1,5 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+//let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+let linkRegex = /whatsapp.com|wa.me|whatsapp.com\/channel/i
 
 export async function before(m, { isAdmin, isBotAdmin }) {
 if (m.isBaileys && m.fromMe)
