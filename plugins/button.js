3fd918242886d5c431475311e92f090d847b1847
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-05 20:43:09 -0500

    Actualizar plugins

---
+++ plugins/button.js
@@ -0,0 +1,12 @@
+let foto = imagen4
+let handler = async (m, { conn, command }) => {
+let user = db.data.users[m.sender]
+let multi = `🌳 Es Mi Creador ⚡️`
+await conn.sendButton(m.chat, multi, `🌼 ¡Hello! ` + nombre, foto, [
+['☄️ Comoleto Menú', '!allmenu'],
+['💚 LoboBot', '!sc'],
+['⌨️ Creador', '#owner'],
+['🛑 Hola Bot', '!bot Hey Wue Mas']], m)}
+handler.customPrefix = /axx|Axx|ax|Ax/i;
+handler.command = new RegExp;
+export default handler;
