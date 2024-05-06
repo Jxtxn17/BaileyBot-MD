ae6387f940772e8bb8a24bc56a1cc9e7523e30ec
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-05 22:53:58 -0500

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, command }) => {
 let user = db.data.users[m.sender]
 let multi = `🌳 Es Mi Creador ⚡️`
 await conn.sendButton(m.chat, multi, `🌼 ¡Hello! ` + nombre, foto, [
-['☄️ Comoleto Menú', '!allmenu'],
-['💚 BaileyBot', '!sc'],
+['☄️ Menu Completo', '!allmenu'],
+['💚 Script', '!sc'],
 ['⌨️ Creador', '#owner'],
 ['🛑 Hola Bot', '!bot Hey Wue Mas']], m, fake)}
 handler.customPrefix = /axx|Axx|ax|Ax/i;
