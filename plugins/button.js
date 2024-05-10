989ad0caabb593d0b0fe2545d964253c557ce284
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-10 09:17:43 -0500

    Actualizar button.js

--- plugins/button.js
+++ plugins/button.js
@@ -4,9 +4,9 @@ let user = db.data.users[m.sender]
 let multi = `🌳 Es Mi Creador ⚡️`
 await conn.sendButton(m.chat, multi, `🌼 ¡Hello! ` + nombre, foto, [
 ['☄️ Completo Menú', '!allmenu'],
-['💚 BaileyBot', '!sc'],
-['⌨️ Creador', '#owner'],
-['🛑 Hola Bot', '!bot como estás']], m)}
+['🍧 BaileyBot', '!sc'],
+['🧸 Creador', '#owner'],
+['🍫 Hola Bot', '!bot como estás']], m)}
 handler.customPrefix = /Axx|axx|ax|Ax/i;
 handler.command = new RegExp;
 export default handler;
