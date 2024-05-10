31051a7c1368427eb61802e4806323a3bb4d0ac9
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-10 10:24:52 -0500

    Actualizar button.js

--- plugins/button.js
+++ plugins/button.js
@@ -1,13 +1,13 @@
 let foto = imagen4
 let handler = async (m, { conn, command }) => {
 let user = db.data.users[m.sender]
-let text = `❤️ Waoz Ese Es Mi Propietario. 🌼`
-await conn.sendButton(m.chat, text, `👋🏻 ¡Hola! ` + nombre, foto, [
-['🎋 Menu De Bailey', '!allmenu'],
-['🌻 Botsito', '!sc'],
-['❤️ Axx', '#owner'],
-['🌼 BaileyBot', '!bot Hola']], null, [
-['🌹 Canal Oficial', `${channel}`]], m)}
+let text = `❤️ Ese es mi propietario.`
+await conn.sendButton(m.chat, text, `🧸 ¿Comó estás? ` + nombre, foto, [
+['ꨄ Menú completo', '!allmenu'],
+['ꨄ BaileyBot-MD', '!sc'],
+['ꨄ Desarrolladores', '#owner'],
+['ꨄ BaileyBot', '!bot Hola']], null, [
+['ꨄ Canal Oficial', `${channel}`]], m)}
 handler.customPrefix = /Axx|Ax/i;
 handler.command = new RegExp;
 export default handler;
