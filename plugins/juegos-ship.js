718a1a04d26c765457972574c7cdf9e0063aa586
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-10 17:43:26 -0500

    Actualizar juegos-ship.js

--- plugins/juegos-ship.js
+++ plugins/juegos-ship.js
@@ -1,10 +1,10 @@
 var handler = async (m, { conn, command, text }) => {
 
-if (!text) return conn.reply(m.chat, `🎌 *Escribe el nombre de dos personas para calcular su amor*`, m, fake, )
+if (!text) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *ᥱsᥴrіᑲᥱ ᥱᥣ ᥒ᥆mᑲrᥱ ძᥱ ძ᥆s ⍴ᥱrs᥆ᥒᥲs ⍴ᥲrᥲ ᥴᥲᥣᥴᥙᥣᥲr sᥙ ᥲm᥆r*`, m, fake, )
 let [text1, ...text2] = text.split(' ')
 
 text2 = (text2 || []).join(' ')
-if (!text2) return conn.reply(m.chat, `🚩 *Escribe el nombre de la segunda persona*`, m, fake, )
+if (!text2) return conn.reply(m.chat, `🚫 𝐀𝐓𝐄𝐍𝐂𝐈𝐎𝐍 🚫 *ᥱsᥴrіᑲᥱ ᥱᥣ ᥒ᥆mᑲrᥱ ძᥱ ᥣᥲ sᥱgᥙᥒძᥲ ⍴ᥱrs᥆ᥒᥲ*`, m, fake, )
 let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de *${Math.floor(Math.random() * 100)}%* 👩🏻‍❤️‍👨🏻`
 
 m.reply(love, null, { mentions: conn.parseMention(love) })
