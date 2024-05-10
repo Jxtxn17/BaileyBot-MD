fb11c3a01c511ace9dc06864481961b0666b3f0a
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-10 09:21:26 -0500

    Actualizar button.js

--- plugins/button.js
+++ plugins/button.js
@@ -2,7 +2,7 @@ let foto = imagen4
 let handler = async (m, { conn, command }) => {
 let user = db.data.users[m.sender]
 let multi = `🌳 Llamas a mi creador?`
-await conn.sendButton(m.chat, multi, `🦝 Como éstas` + nombre, foto, [
+await conn.sendButton(m.chat, multi, `🤔 Como éstas ` + nombre, foto, [
 ['☄️ Completo Menú', '!allmenu'],
 ['🍧 BaileyBot', '!sc'],
 ['🧸 Creador', '#owner'],
