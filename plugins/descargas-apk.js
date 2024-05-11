63e2b754f5f628e5b8662dea346a6a72f3ec699a
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-11 12:54:18 -0500

    Actualizar plugins

---
+++ plugins/descargas-apk.js
@@ -0,0 +1,15 @@
+import { search, download } from 'aptoide-scraper'
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+if (!text) throw `*Formato incorrecto*\nEjemplo:\n${usedPrefix + command} Pokemon Go`
+try {
+const searchResult = await search(text)
+const data = await download(searchResult[0].id)
+if (data.size.includes('GB') || parseFloat(data.size.replace(' MB', '')) > 300) {
+return await conn.sendMessage(m.chat, { text: '*El archivo es demasiado pesado por lo que no se enviará.*' }, { quoted: m })
+}
+await conn.sendMessage(m.chat, { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null }, { quoted: m })
+} catch {
+}}
+handler.command = /^(apk)$/i
+handler.limit = 5
+export default handler
