79e3233674d5c2f1125f0b30fe53588799c562b0
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-11 12:55:36 -0500

    Actualizar descargas-apk.js

--- plugins/descargas-apk.js
+++ plugins/descargas-apk.js
@@ -1,6 +1,6 @@
 import { search, download } from 'aptoide-scraper'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-if (!text) throw `*Formato incorrecto*\nEjemplo:\n${usedPrefix + command} Pokemon Go`
+if (!text) throw `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫*\n[ 💡 ] ᥱȷᥱm⍴ᥣ᥆ ${usedPrefix + command} WhatsApp Plus`
 try {
 const searchResult = await search(text)
 const data = await download(searchResult[0].id)
