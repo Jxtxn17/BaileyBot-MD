76ac5e1336097b6fa06d245e2b2d756a250b5eb4
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-12 11:49:39 -0500

    Actualizar bot-prueba.js

--- plugins/bot-prueba.js
+++ plugins/bot-prueba.js
@@ -1,4 +1,17 @@
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+import ytdl from 'ytdl-core'
+import axios from 'axios'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 
+var handler = async (m, { conn, command, args, text, usedPrefix }) => {
+
+if (!text) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥱ ᥱᥣ ᥒ᥆mᑲrᥱ ძᥱ ᥙᥒ ᥎іძᥱ᥆ ძᥱ ᥡ᥆ᥙ𝗍ᥙᑲᥱ*\n\n[ 💡 ] ᥱȷᥱm⍴ᥣ᥆, !${command} New West - Those Eyes`,  m, fake, )
+m.react(rwait)
+
+try {
+
+case 'qqq':
 if (!text) return m.reply('Ejemplo, play photograph')
 const search = await yts(`${text}`)
 const ytMsg = `Título: ${search.all[0].title}\nVistas: ${search.all[0].views}\nUrl: ${search.videos[0].url}`
