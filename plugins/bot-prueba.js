e67b8f62e80055ee7b2e7959acb7e3c14710b4ef
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-12 12:46:10 -0500

    Actualizar bot-prueba.js

--- plugins/bot-prueba.js
+++ plugins/bot-prueba.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 }
 handler.help = ['play']
 handler.tags = ['dl']
-handler.command = ['play', 'playvid']
+handler.command = ['play', 'playqq']
 handler.disabled = true
 
 export default handler
