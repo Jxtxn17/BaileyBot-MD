1a5b4f26c4dfa68d49ea70715a64e0850cd8b560
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-17 18:49:47 -0500

    Actualizar plugins

---
+++ plugins/rpg-cofre.js
@@ -0,0 +1,76 @@
+const handler = async (m, {isPrems, conn}) => {
+  const time = global.db.data.users[m.sender].lastcofre + 86400000; // 36000000 10 Horas //86400000 24 Horas
+  if (new Date - global.db.data.users[m.sender].lastcofre < 86400000) throw `[❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\n𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`;
+
+  const img = 'https://img.freepik.com/vector-gratis/cofre-monedas-oro-piedras-preciosas-cristales-trofeo_107791-7769.jpg?w=2000';
+  const dia = Math.floor(Math.random() * 30);
+  const tok = Math.floor(Math.random() * 10);
+  const mystic = Math.floor(Math.random() * 4000);
+  const expp = Math.floor(Math.random() * 5000);
+
+  global.db.data.users[m.sender].limit += dia;
+  global.db.data.users[m.sender].money += mystic;
+  global.db.data.users[m.sender].joincount += tok;
+  global.db.data.users[m.sender].exp += expp;
+
+  const texto = `
+╔══🪁═🪁═🪁══⬣
+║🧨 𝙾𝙱𝚃𝙸𝙴𝙽𝙴𝚂 𝚄𝙽 𝙲𝙾𝙵𝚁𝙴
+║┈┈┈┈┈┈┈┈┈┈┈┈┈
+║✒ *${dia} 𝘿𝙞𝙖𝙢𝙖𝙣𝙩𝙚𝙨* 💎
+║✒ *${tok} 𝙏𝙤𝙠𝙚𝙣𝙨* 🪙
+║✒ *${expp} 𝙀𝙭𝙥* ⚡
+╚═════════════════⬣`;
+
+  const fkontak = {
+    'key': {
+      'participants': '0@s.whatsapp.net',
+      'remoteJid': 'status@broadcast',
+      'fromMe': false,
+      'id': 'Halo',
+    },
+    'message': {
+      'contactMessage': {
+        'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+      },
+    },
+    'participant': '0@s.whatsapp.net',
+  };
+ await conn.sendMessage(m.chat, {
+text: texto,
+contextInfo: {
+externalAdReply: {
+title: wm,
+body: 'sɪɢᴜᴇᴍᴇ ᴇɴ ɪɴsᴛᴀɢʀᴀᴍ',
+sourceUrl: md,
+thumbnailUrl: img, 
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: true
+}}} , { quoted: m });
+  //await conn.sendFile(m.chat, img, 'hachiko.jpg', texto, fkontak);
+
+  global.db.data.users[m.sender].lastcofre = new Date * 1;
+};
+handler.help = ['daily'];
+handler.tags = ['xp'];
+handler.command = ['coffer', 'cofre', 'abrircofre', 'cofreabrir'];
+handler.level = 5;
+export default handler;
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+function msToTime(duration) {
+  const milliseconds = parseInt((duration % 1000) / 100);
+  let seconds = Math.floor((duration / 1000) % 60);
+  let minutes = Math.floor((duration / (1000 * 60)) % 60);
+  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+  return hours + ' Horas ' + minutes + ' Minutos';
+}
