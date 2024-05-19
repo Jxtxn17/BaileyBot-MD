b8c8504191cd18f86b3311bc9e79c9cbe97bd2a7
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-19 14:32:55 -0300

    Actualizar button.js

--- plugins/button.js
+++ plugins/button.js
@@ -4,10 +4,10 @@ let user = db.data.users[m.sender]
 let text = `❤️ Ese es mi propietario.`
 await conn.sendButton(m.chat, text, `🧸 ¿Comó estás? ` + nombre, foto,[
 ['ꨄ Menú completo', '!allmenu'],
-['ꨄ BaileyBot-MD', '!sc'],
+['ꨄ 𝑨𝒏𝒂𝒃𝒊𝑩𝒐𝒕-𝑴𝑫', '!sc'],
 ['ꨄ Desarrolladores', '#owner'],
-['ꨄ BaileyBot', '!bot Hola']], null, [
+['ꨄ 𝐇𝐚𝐧𝐚𝐛𝐢𝐁𝐨𝐭', '!bot Hola']], null, [
 ['ꨄ Canal Oficial', `${channel}`]], m)}
-handler.customPrefix = /Axx|Ax/i;
+handler.customPrefix = /bot|Bot/i;
 handler.command = new RegExp;
 export default handler;
