39d5669cb4b61b5a6b9310f4ff4cc059ae7c87fb
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-19 14:35:23 -0300

    Actualizar bot-bot.js

--- plugins/bot-bot.js
+++ plugins/bot-bot.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) {
 
 conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, '*Hola, soy BaileyBot-MD*\n*Estoy aquí para ayudarte*', m, fake, )}
+conn.reply(m.chat, '*Hola, soy HanabiBot-MD*\n*Estoy aquí para ayudarte*', m, fake, )}
 
 return !0
 
