13e5065ad9f7af937bcf71c4c68ecda0efd1d0ad
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-19 20:11:40 -0300

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
  global.owner = [
-['5491130182250', 'EnzitoOFC', true]]
+['5491130182250', '🍁 𝗢𝘄𝗻𝗲𝗿 - 𝗖𝗹𝗲𝗮𝘁𝗼𝗿 🐼', true]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
