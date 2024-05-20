21d4e65cd3d20407647bbaa1ec871b9cd970069c
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-20 04:20:39 -0300

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 import instagramGetUrl from 'instagram-url-direct'
 import { instagram } from '@xct007/frieren-scraper'
 import { instagramdl } from '@bochilteam/scraper'
- 
+
 var handler = async (m, {conn, args, command, usedPrefix}) => {
- 
-if (!args[0]) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥱ ᥙᥒ ᥱᥒᥣᥲᥴᥱ ძᥱ іᥒs𝗍ᥲgrᥲm*\n\n[ 💡 ] ᥱȷᥱm⍴ᥣ᥆ ${usedPrefix + command} https://www.instagram.com/reel/CuqAzGRAbZa/?igshid=MzRlODBiNWFlZA==`, m, fake, )
-conn.reply(m.chat, `🚫 𝐀𝐓𝐄𝐍𝐂𝐈𝐎𝐍 🚫 ᥱs⍴ᥱrᥱ ᥙᥒ m᥆mᥱᥒ𝗍᥆`, m, fake, )
+
+if (!args[0]) return conn.reply(m.chat, `📱 *Ingrese un enlace de instagram*\n\nEjemplo ${usedPrefix + command} https://www.instagram.com/reel/CuqAzGRAbZa/?igshid=MzRlODBiNWFlZA==`, m, fake, )
+conn.reply(m.chat, `⏰ ${nombre} Espere un momento`, m, fake, )
 
 try {
 
@@ -16,7 +16,7 @@ let responsel = await axios.get(apiUrll)
 let resultl = responsel.data
 for (const item of resultl.message) {
 let shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${item.thumbnail}`)).text()
-let tXXxt = `🍧 *Url:* ${shortUrRRl}`.trim()
+let tXXxt = `📱 *Url:* ${shortUrRRl}\n\n${wm}`.trim()
 conn.sendFile(m.chat, item._url, null, tXXxt, fkontak, m)
 await new Promise((resolve) => setTimeout(resolve, 10000))
 } 
@@ -25,7 +25,7 @@ try {
 let datTa = await instagram.v1(args[0])
 for (const urRRl of datTa) {
 let shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let tXXxt = `🍧 *Url:* ${shortUrRRl}`.trim()
+let tXXxt = `📱 *Url:* ${shortUrRRl}\n\n${wm}`.trim()
 conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, fkontak, m)
 await new Promise((resolve) => setTimeout(resolve, 10000))
 }
@@ -33,13 +33,13 @@ await new Promise((resolve) => setTimeout(resolve, 10000))
 try {
 let resultss = await instagramGetUrl(args[0]).url_list[0]
 let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let txt2 = `🍧 *Url:* ${shortUrl2}`.trim()
+let txt2 = `📱 *Url:* ${shortUrl2}\n\n${wm}`.trim()
 await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
 } catch {
 try {
 let resultssss = await instagramdl(args[0])
 let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let txt4 = `🍧 *Url:* ${shortUrl3}`.trim()
+let txt4 = `📱 *Url:* ${shortUrl3}\n\n${wm}`.trim()
 for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
 } catch {
 try {
@@ -47,10 +47,10 @@ let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeys
 let json = await human.json()
 let videoig = json.result
 let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-let txt1 = `🍧 *Url:* ${shortUrl1}`.trim()
+let txt1 = `📱 *Url:* ${shortUrl1}\n\n${wm}`.trim()
 await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
 } catch {
-return conn.reply(m.chat, '🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *᥆ᥴᥙrrі᥆ ᥙᥒ 𝖿ᥲᥣᥣᥣ*', m, fake, )
+return conn.reply(m.chat, '⌨️ *Ocurrió un fallo*', m, fake, )
 }}}}} 
 
 }
@@ -59,6 +59,5 @@ handler.tags = ['descargas']
 handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
 
 handler.register = true
-handler.limit = true
 
 export default handler
