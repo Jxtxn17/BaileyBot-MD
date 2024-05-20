5c9f97c91e5893a685384ce89734212af2855493
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-20 04:12:08 -0300

    Add files via upload

---
+++ plugins/downloader-ytv.2.js
@@ -0,0 +1,31 @@
+import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
+import fetch from 'node-fetch';
+const handler = async (m, {conn, args}) => {
+  if (!args[0]) throw '*Y EL LINK?*';
+  await m.reply(`*_⏳ Procesando Video...⏳_*\n\n*Si el video no se envia pruebe los siguientes comandos #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`);
+  try {
+    const qu = args[1] || '360';
+    const q = qu + 'p';
+    const v = args[0];
+    const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
+    const dl_url = await yt.video[q].download();
+    const ttl = await yt.title;
+    const size = await yt.video[q].fileSizeH;
+    const cap = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${ttl}\n❏ *𝙿𝙴𝚂𝙾:* ${size}\n✰ 𝗬𝗼𝘀𝗵𝗶𝗸𝗼 𝗕𝗼𝘁 - 𝗠𝗗 ★`.trim();
+    await await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m});
+  } catch {
+    try {
+      const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${args[0]}`);
+      const lolh = await lolhuman.json();
+      const n = lolh.result.title || 'error';
+      const n2 = lolh.result.link;
+      const n3 = lolh.result.size;
+      const cap2 = `*◉—⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋—◉*\n❏ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${n}\n❏ *𝙿𝙴𝚂𝙾:* ${n3}\n✰ 𝗬𝗼𝘀𝗵𝗶𝗸𝗼 𝗕𝗼𝘁 - 𝗠𝗗 ★`.trim();
+      await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'video/mp4', fileName: n + `.mp4`}, {quoted: m});
+    } catch {
+      await conn.reply(m.chat, '*⚠️ No se descargó el video*', m);
+    }
+  }
+};
+handler.command = /^ytmp4doc|ytvdoc|ytmp4.2|ytv.2$/i;
+export default handler;
