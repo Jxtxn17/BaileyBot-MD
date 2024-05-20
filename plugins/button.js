60c73f29dbdf29b06d609c3ada4f575ec9b69e39
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-20 02:33:33 -0300

    Actualizar button.js

--- plugins/button.js
+++ plugins/button.js
@@ -4,9 +4,9 @@ let user = db.data.users[m.sender]
 let text = `❤️ Ese es mi propietario.`
 await conn.sendButton(m.chat, text, `🧸 ¿Comó estás? ` + nombre, foto,[
 ['ꨄ Menú completo', '!allmenu'],
-['ꨄ 𝑨𝒏𝒂𝒃𝒊𝑩𝒐𝒕-𝑴𝑫', '!sc'],
+['ꨄ DiablaBot-MD', '!sc'],
 ['ꨄ Desarrolladores', '#owner'],
-['ꨄ 𝐇𝐚𝐧𝐚𝐛𝐢𝐁𝐨𝐭', '!bot Hola']], null, [
+['ꨄ DiablaBot', '.bot Hola']], null, [
 ['ꨄ Canal Oficial', `${channel}`]], m)}
 handler.customPrefix = /Enzito|Enzo/i;
 handler.command = new RegExp;
