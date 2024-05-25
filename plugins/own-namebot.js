105a967bdb407609949bf9b6255f0e8754d6d403
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-25 12:05:54 -0500

    Actualizar own-namebot.js

--- plugins/own-namebot.js
+++ plugins/own-namebot.js
@@ -1,19 +1,17 @@
-var handler = async (m, { conn, text, usedPrefix, command }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => { 
+   if (!text) throw `¿𝙲𝚄𝙰𝙻 𝙴𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝚀𝚄𝙴 𝚃𝙴𝙽𝙳𝚁𝙴?` 
+   try { 
+     await conn.updateProfileName(text) 
+     m.reply('𝙻𝙸𝚂𝚃𝙾, 𝙽𝙾𝙼𝙱𝚁𝙴 𝙲𝙰𝙼𝙱𝙸𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾🪁!') 
+   } catch (e) { 
+     console.log(e) 
+     throw `[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁 𝙰𝙻 𝙲𝙰𝙼𝙱𝙸𝙰𝚁 𝙴𝙻 𝙽𝙰𝙼𝙴` 
+   } 
+ } 
+ handler.help = ['setbotname <teks>'] 
+ handler.tags = ['owner'] 
+ handler.command = /^(setbotname|cambianombre)$/i 
 
-if (!text) return conn.reply(m.chat, '🎌 *Ingresa el nick (nombre)*\n\nEjemplo, !setnamebot Curiosity', m, fake, )
-try {
-await conn.updateProfileName(text)
-conn.reply(m.chat, '✅ *Se actualizó el nombre con éxito*', m, fake, )
-} catch (e) {
-console.log(e)
-return conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, )
-}
+ handler.owner = true 
 
-}
-handler.help = ['setbotname']
-handler.tags = ['own']
-handler.command = /^(setbotname|cambianombre)$/i
-
-handler.owner = true
-
-export default handler
+ export default handler
