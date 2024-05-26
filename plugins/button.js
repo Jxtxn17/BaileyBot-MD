2d225a9235303d61471afd468e15cae22be38218
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-25 20:26:11 -0500

    recargoso

--- plugins/button.js
+++ plugins/button.js
@@ -1,13 +1,1 @@
-/*let foto = imagen4
-let handler = async (m, { conn, command }) => {
-let user = db.data.users[m.sender]
-let text = `❤️ Ese es mi propietario.`
-await conn.sendButton(m.chat, text, `🧸 ¿Comó estás? ` + nombre, foto,[
-['ꨄ Menú completo', '!allmenu'],
-['ꨄ BaileyBot-MD', '!sc'],
-['ꨄ Desarrolladores', '#owner'],
-['ꨄ BaileyBot', '!bot Hola']], null, [
-['ꨄ Canal Oficial', `${channel}`]], m)}
-handler.customPrefix = /Axx|Ax/i;
-handler.command = new RegExp;
-export default handler;*/
+
