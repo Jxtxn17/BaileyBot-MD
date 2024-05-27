c8fb38378ccc6623b0ac78e0137e9048b09685e3
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-27 12:27:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -310,7 +310,7 @@ if (opcion == '1') {
 console.log(chalk.yellow('⚠️ㅤEscanea este codigo QR, el codigo QR expira en 60 segundos.'))
  }}
 if (connection == 'open') {
-console.log(chalk.yellowBright('\n╭┈ ┈ ┈ ┈ ┈ • Bailey𝗕𝗼𝘁-𝗠𝗗 🍂 • ┈ ┈ ┈ ┈ ┈ ┈╮\n┊ EL BOT SE CONECTO CORRECTAMENTE AL WHATSAPP 🟢\n╰┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈ ┈╯\n'))}
+console.log(chalk.bold.cyan('\n╭───────────────────────────◉ • ${wm} 🍂 • ───────────────────────────╮\n┊ El Bot Se Conecto Correctamente al WhatsApp 🟢\n╰───────────────────────────◉╯\n'))}
 //if (conn.user.connect) {
 //conn.fakeReply('5217294888993@s.whatsapp.net', '😃', '0@s.whatsapp.net', '😅 Soy CuriosityBot\nRecientemente me e conectado', '0@s.whatsapp.net')
 //conn.user.connect = true;
