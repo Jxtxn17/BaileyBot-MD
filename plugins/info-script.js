d12f24b410ca1770c47275eb12ef02e6f244cc91
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-06-01 00:02:09 -0500

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
    const res = await fetch('https://api.github.com/repos/Jxtxn17/BaileyBot-MD');
    const json = await res.json();
-   let txt = `           *乂  B O T S C R I P T  乂*\n\n`;
+   let txt = `           *乂  B O T - S C R I P T  乂*\n\n`;
       txt += `◦  *Nombre:* ${json?.name || 'BaileyBot-MD'}\n`;
       txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
       txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
