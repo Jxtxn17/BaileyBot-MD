410f4ecbe510725decb86b261e99f270b44482d9
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-05-31 23:58:11 -0500

    Update info-script2.js

--- plugins/info-script2.js
+++ plugins/info-script2.js
@@ -1,13 +1,18 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
-   const res = await fetch('https://api.github.com/repos/Diego-YL-177/YoshikoBot-MD');
+   const res = await fetch('https://api.github.com/repos/Jxtxn17/BaileyBot-MD');
    const json = await res.json();
-   let txt =          `*乂  B O T  -  S C R I P T*\n\n◦ *Nombre* : ${json.name}\n◦ *Visitantes* : ${json.watchers_count}\n◦ *Peso* : ${(json.size / 1024).toFixed(2)} MB\n◦ *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n◦ *Url* : ${json.html_url}\n\n	   ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n\n> By BaileyBot-MD`
-await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen2, thumbnailUrl: imagen1, title: '\t\t\t\t\t\t᭡͡ᩬ🍧✩̣̣̣̣̣ͯ𝐁𝐚𝐢𝐥𝐞𝐲𝑩𝒐𝒕-𝑴𝑫᭡͡ᩬ🍧✩̣̣̣̣̣ͯ', }})
- 
+   let txt = `           *乂  B O T S C R I P T  乂*\n\n`;
+      txt += `◦  *Nombre:* ${json?.name || 'BaileyBot-MD'}\n`;
+      txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
+      txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
+      txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
+      txt += `◦  *Url:* ${json?.html_url || 'https://github.com/Jxtxn17/BaileyBot-MD'}\n\n`;
+      txt += `         ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
+   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen4, "mediaUrl": `https://github.com/Jxtxn17/BaileyBot-MD`, "sourceUrl": `https://github.com/Jxtxn17/BaileyBot-MD`}}}, {quoted: fkontak});
 };
 handler.help = ['scbot'];
 handler.tags = ['info'];
-handler.command = /^(sc|scbot|aa|script)$/i;
+handler.command = /^(sc|scbot|scrip|script)$/i;
 export default handler;
