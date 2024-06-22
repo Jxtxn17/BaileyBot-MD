573d91f708bf85419c6fd60b340ea6ca8ac69a15
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-06-22 02:03:53 -0500

    Actualizar bot-ping.js

--- plugins/bot-ping.js
+++ plugins/bot-ping.js
@@ -4,7 +4,7 @@ import util from 'util'
 import osu from 'node-os-utils'
 import { performance } from 'perf_hooks'
 import { sizeFormatter } from 'human-readable'
-//import speed from 'performance-now'
+import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 const format = sizeFormatter({ std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B` })
 
