fff69b156eba67569ea6d1be5ca0b7bc9cc1bbc4
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-25 15:01:36 -0500

    Actualizar bot-bot.js

--- plugins/bot-bot.js
+++ plugins/bot-bot.js
@@ -1,14 +1,27 @@
-var handler = m => m
-handler.all = async function (m) {
+import fs from 'fs';
+const handler = (m) => m;
+handler.all = async function(m) {
 
-let chat = global.db.data.chats[m.chat]
-
-if (/^bot$/i.test(m.text) && !chat.isBanned) {
-
-conn.sendPresenceUpdate('composing', m.chat)    
-conn.reply(m.chat, '*Hola, soy BaileyBot-MD*\n*Estoy aquí para ayudarte*', m, fake, )}
-
-return !0
-
+const chat = global.db.data.chats[m.chat];
+if (chat.isBaneed) return
+if (/^bot$/i.test(m.text)) {
+conn.reply(m.chat, `🥷 ¡Hola! Soy ${global.packname}, en que puedo ayudarte hoy?\n✰ Usa */menu* para ver mis comandos.`, m, fake, )
 }
-export default handler
+if (/^que|q$/i.test(m.text)) {
+conn.reply(m.chat, `*so* 🧀`, m, fake, )
+}
+if (/^a$/i.test(m.text)) {
+conn.reply(m.chat, `*rrocito* 🍚`, m, fake, )
+}
+if (/^sexo$/i.test(m.text)) {
+conn.reply(m.chat, `*pervertido* 🫣`, m, fake, )
+}
+if (/^bug$/i.test(m.text)) {
+conn.reply(m.chat, `*tu mamá we* 😹`, m, fake, )
+}
+if (/^pene$/i.test(m.text)) {
+conn.reply(m.chat, `*comes* 😹`, m, fake, )
+}
+return !0;
+};
+export default handler;
