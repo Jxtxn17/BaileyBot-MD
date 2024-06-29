c093c8af45282cb356feca437879cfcd9cc4279f
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-06-29 14:42:45 -0500

    Actualizar store.js

--- store.js
+++ store.js
@@ -147,6 +147,8 @@ global.APIKeys = {
 
 //Rpg
 global.multiplier = 200 // Cuanto más alto, más difícil subir de nivel
+global.db.data.users[m.sender].money += coins;
+global.db.data.users[m.sender].joincount += tok;
 
 global.rpg = {
 emoticon(string) {
