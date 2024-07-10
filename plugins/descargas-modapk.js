684df331921da93d06da58d0c930480aafc1626f
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-07-10 00:10:42 -0500

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -2,7 +2,7 @@ import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, {conn, usedPrefix, command, text}) => {
 
-if (!text) return conn.reply(m.chat, '🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥱ ᥱᥣ ᥒ᥆mᑲrᥱ ძᥱ ᥣᥲ ᥲ⍴k 𝗊ᥙᥱ ᥱs𝗍ᥲ ᑲᥙsᥴᥲᥒძ᥆*', m, fake, )
+if (!text) return conn.reply(m.chat, '🍟 *Ingres@ el nombre del Apk que esta buscando*', m, fake, )
 
 try {
  
@@ -12,11 +12,11 @@ let response = `💌 *Nombre:* ${data5.name}\n📦 *Paquete:* ${data5.package}\n
 await conn.sendMessage(m.chat, { text: response, contextInfo: { externalAdReply: { title: data5.name, body: wm, sourceUrl: md, thumbnailUrl: data5.icon, mediaType: 1, showAdAttribution: true, renderLargerThumbnail: true }}} , { quoted: m })   
 
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, '🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *ᥱᥣ ᥲrᥴһі᥎᥆ ᥱs ძᥱmᥲᥴіᥲძ᥆ ⍴ᥱsᥲძ᥆*', m, fake, )
+return await conn.reply(m.chat, '❗ *El archivo es demasiado pesado*', m, fake, )
 }
 await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m})
 } catch {
-return conn.reply(m.chat, '🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *᥆ᥴᥙrrі᥆́ ᥙᥒ 𝖿ᥲᥣᥣ᥆*', m, fake, )
+return conn.reply(m.chat, '「👑」 *Ocurrio un fallo*', m, fake, )
 }    
 }
 
