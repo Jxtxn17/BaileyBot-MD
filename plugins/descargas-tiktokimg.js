98579e998d1d5a6670e91d07054340e63a5b2783
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-07-10 00:04:13 -0500

    Actualizar descargas-tiktokimg.js

--- plugins/descargas-tiktokimg.js
+++ plugins/descargas-tiktokimg.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 
 var handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥱ ᥙᥒ ᥱᥒᥣᥲᥴᥱ ძᥱ 𝗍іk𝗍᥆k 𝗊ᥙᥱ ᥴ᥆ᥒ𝗍ᥱᥒgᥲ іmᥲ́gᥱᥒᥱs*`, m, fake, )
+if (!text) return conn.reply(m.chat, `🍿 *Ingrese un enlace de tiktok que contenga imágenes*`, m, fake, )
 m.react(rwait)
-if (!(text.includes('http://') || text.includes('https://'))) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *ᥱs𝗍ᥱ ᥱᥒᥣᥲᥴᥱ ᥒ᥆ ᥴ᥆ᥒ𝗍іᥱᥒᥱ http:// ni https://*`, m, fake, )
+if (!(text.includes('http://') || text.includes('https://'))) return conn.reply(m.chat, `🍟 *Este enlace no contiene http:// ni https://*`, m, fake, )
 m.react(error)
-if (!text.includes('tiktok.com')) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *ᥙrᥣ ᥱrr᥆́ᥒᥱᥲ*`, m, fake, )
+if (!text.includes('tiktok.com')) return conn.reply(m.chat, `🍭 *Enlace erróneo*`, m, fake, )
 m.react(error)
 
 try {
@@ -18,7 +18,7 @@ anu = anu.result
 if (anu.length == 0) throw Error('Error : no data')
 let c = 0
 for (let x of anu) {
-if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `🚫 𝐀𝐓𝐄𝐍𝐂𝐈𝐎𝐍 🚫 *sᥱ һᥲ ᥱᥒ᥎іᥲძ᥆ 1 ძᥱ ${anu.length} іmᥲ́gᥱᥒᥱs.*\n_Eᥣ rᥱs𝗍᥆ ⍴᥆ძrᥲ sᥱr ᥎іsіᑲᥣᥱ ᥱᥒ ᥱᥣ ᥴһᥲ𝗍 ⍴rі᥎ᥲძ᥆ ძᥱᥣ bot_` }, { quoted : m })
+if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `🍔 *Se ha enviado 1 de {anu.length} imágenes.*\n_El resto será visible en el chat privado del bot_` }, { quoted : m })
 else await conn.sendMessage(m.sender, { image: { url: x } }, { quoted : m })
 c += 1
 }
@@ -26,7 +26,7 @@ m.react(done)
 } catch (e) {
 console.log(e)
 m.react(error)
-return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *᥆ᥴᥙrrі᥆́ ᥙᥒ 𝖿ᥲᥣᥣ᥆*`, m, fake, )
+return conn.reply(m.chat, `🍧 *Ocurrio un falló*`, m, fake, )
 }
 
 }
