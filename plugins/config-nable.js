bcab1e989d8dcc2042878389d944231e2ee5bea1
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-07-12 18:50:22 -0500

    Update config-nable.js

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -170,14 +170,6 @@ throw false
 }}
 chat.modohorny = isEnable
 break
-case 'prueba234':
-if (m.isGroup) {
-if (!(isAdmin || isOwner)) {
-global.dfail('admin', m, conn)
-throw false
-}}
-chat.prueba234 = isEnable
-break
 case 'autosticker':
 if (m.isGroup) {
 if (!(isAdmin || isOwner)) {
