e11aa880b63d401b360b8da75e2b76f3c4785e6a
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-12 18:25:19 -0500

    Actualizar bot-ping.js

--- plugins/bot-ping.js
+++ plugins/bot-ping.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          m.reply(`*Pong* 🏓 ${latensi.toFixed(4)} ms`);
+          conn.reply(m.chat, `*Pong* 🏓 ${latensi.toFixed(4)} ms`, m, fake, );
             });
 }
 handler.help = ['ping']
