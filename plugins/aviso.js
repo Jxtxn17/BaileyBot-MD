69fd976fb6d4f31e870c17807ad426fcef807a20
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-08-02 19:08:53 -0500

    Update aviso.js

--- plugins/aviso.js
+++ plugins/aviso.js
@@ -1,13 +1,13 @@
 var handler = async (m, { conn, command }) => {
 
-let str = `¡Usa el comando .play!`
+let str = `¡Usa el comando .Play!`
 
 conn.reply(m.chat, str, m, fake, )
 
 }
 handler.help = ['aviso']
 handler.tags = ['info']
-handler.command = /^aviso|play2|$/i
+handler.command = /^aviso|play2$/i
 
 handler.register = true
 
