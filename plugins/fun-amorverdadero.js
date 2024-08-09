e15d44df816bac212c1b96963eac342b25783926
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-08-08 21:58:43 -0500

    Actualizar fun-amorverdadero.js

--- plugins/fun-amorverdadero.js
+++ plugins/fun-amorverdadero.js
@@ -1,23 +1,12 @@
-/*
-🍧 Codigo Realizado Por : 
-• OfcDiego (https://github.com/OfcDiego)
-*/
-
 let handler = async(m, { conn, text, command, usedPrefix }) => {
 
-//Envia para que escriba el nombre de las 2 personas, no pongas esto: .amorverdadero Alejandro y Maria pon solo: .amorverdadero Alejandro Maria
 if (!text) return conn.reply(m.chat, '⚜️ Te faltó el nombre de 2 personas...', m, fake, )
 let [text1, ...text2] = text.split(' ')
 
-//Son 2 nombres..
 text2 = (text2 || []).join(' ')
 if (!text2) return conn.reply(m.chat, '⚜️ Son 2 nombres por favor...', m, fake, )
 
-//Text de donde envia el mensaje.
 let amor = `*${text1} Busca A: ${text2} Para Que L@ Mantenga* 🥺`
-
-//Envia el mensaje!
-// conn.reply(m.chat, amor, m, fake, )
 
 m.reply(amor, null, { mentions: conn.parseMention(amor) })
 
