3bb004486b77265c21adea93ccadc52ed12a1494
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-08-09 21:07:14 -0500

    Update bot-bot.js

--- plugins/bot-bot.js
+++ plugins/bot-bot.js
@@ -7,7 +7,7 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🥷 ¡Hola! Soy ${global.packname}, en que puedo ayudarte hoy?\n✰ Usa */menu* para ver mis comandos.`, m, fake, )
 }
-if (/^que|q$/i.test(m.text)) {
+/*if (/^que|q$/i.test(m.text)) {
 conn.reply(m.chat, `*so* 🧀`, m, fake, )
 }
 if (/^a$/i.test(m.text)) {
@@ -21,7 +21,7 @@ conn.reply(m.chat, `*tu mamá we* 😹`, m, fake, )
 }
 if (/^pene$/i.test(m.text)) {
 conn.reply(m.chat, `*comes* 😹`, m, fake, )
-}
+}*/
 return !0;
 };
 export default handler;
