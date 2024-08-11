e596a725be4ad359a16e55963c05a51ac91803ca
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-08-11 00:43:15 -0500

    Update store.js

--- store.js
+++ store.js
@@ -1,7 +1,6 @@
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import fs from 'fs'
-import cheerio from 'cheerio'
 import fetch from 'node-fetch'
 import axios from 'axios'
 import path, { join } from 'path'
